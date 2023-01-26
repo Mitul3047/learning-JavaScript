3b808614195a69d362e59f4807c09d96f75c8277
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-01-26 13:25:23 +0600

    Update array2.js

--- array2.js
+++ array2.js
@@ -5,3 +5,11 @@ tealine.push('julam')
 console.log(tealine)
 tealine.pop()
 console.log(tealine)
+tealine.unshift('palam')
+console.log(tealine)
+var part = tealine.slice(2)
+console.log(part)
+var part = tealine.slice(2,4)
+console.log(part)
+
+
