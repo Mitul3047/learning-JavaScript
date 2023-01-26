dc30006533978a10aff70055ef71af84b97392e6
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-01-26 14:51:30 +0600

    Create forloop.js

---
+++ forloop.js
@@ -0,0 +1,17 @@
+// var y =0
+// while (y<18) {
+//     console.log(y)
+//     i++
+// }
+
+// for(var i = 0; i<9; i++){
+//     console.log(i)
+// }
+
+var num = [55, 66, 77, 89, 99, 11, 44];
+
+for (let i = 0; i < num.length; i++) {
+    var element = num[i];
+    console.log(element)
+    
+}
