579b01a5fc68a008f57db18db183496ed2ce7afa
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-01-27 11:51:11 +0600

    Switch

---
+++ switch.js
@@ -0,0 +1,39 @@
+// num =5 
+// if (num > 1000) {
+    
+// }
+// else if (num > 100) {
+    
+// } 
+// else if (num > 50) {
+    
+// } 
+// else if (num > 20) {
+    
+// } 
+// else if (num > 10) {
+    
+// } 
+// else if (num > 5) {
+    
+// } 
+// else if (num > 0) {
+    
+// } 
+// else {
+    
+// }
+
+num = 5
+switch (num) {
+    case 1000:
+        console.log('i am 1000')
+        break;
+    case 100:
+        console.log('i am 100')
+        break;
+
+    default:
+        console.log(`i dont know`)
+        break;
+}
